15e5dcc3656e5b49233f0db83615a51f123de678
azimezn <117550865+azimezn@users.noreply.github.com>
2023-06-08 10:51:28 -0400

    added mongodb and connected to atlas. added dotenv. made sure the page doesnt refresh after submit.

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,23 @@
-console.log("hellooooo");
+// event listener for the element with id= "myForm"
+$("#myForm").submit(function (event) {
+    // prevents the page from refreshing
+    event.preventDefault();
+
+    // collects all of the form data
+    // this = the form
+    var formData = $(this).serialize();
+
+    $.ajax({
+        url: "/formSubmit",
+        type: "POST",
+        data: formData,
+        success: function (response) {
+            console.log(response);
+            alert("Data saved successfully");
+        },
+        error: function (xhr, status, error) {
+            console.log(xhr);
+            alert("An error occurred: " + status + "nError: " + error);
+        }
+    });
+});
--- server.js
+++ server.js
@@ -1,13 +1,68 @@
-// npm i express
+require('dotenv').config();
+
+// express for backend
 const express = require('express');
-// path is a standard package
-const path = require('path');
+// mongodb to store data
+const mongodb = require('mongodb').MongoClient;
 
 // express() is a global function
 const app = express();
+
 // localhost:3001
 const PORT = 3001;
 
+// connection string to local instance of monggodb, including database name
+const connectionStringURI = `mongodb+srv://mongo:${process.env.MONGO_PASSWORD}@azimebirthday.xkfugto.mongodb.net/birthdayDB?retryWrites=true&w=majority`;
+
+// declare a variable to hold the connection
+let db;
+
+// connects to mongodb and returns the reference to the database
+mongodb.connect(
+    connectionStringURI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, client) => {
+        db = client.db();
+        app.listen(PORT, () => {
+            console.log(`Example app listening at http:://localhost:${PORT}`)
+        })
+    }
+)
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // app = express
 // letting express know that the 'public' folder has static content
+// static means that it is served in the browser. such as images, css, js. it is not processed by the server.
 app.use(express.static('public'));
+
+// get <-- method
+// '/' <-- path/endpoint
+// (req, res) <-- handler - function executed - callback
+app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+
+// '/send' <-- path/endpoint
+// res = response
+app.get('/send', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/sendFile.html'))
+);
+
+app.get('/routes', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/routes.html'))
+);
+
+app.post('/formSubmit', (req, res) => {
+    // 'formResponses' is the name of the collection
+    db.collection('formResponses').insertOne(
+        req.body,
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send("Error while saving to database");
+            } else {
+                console.log(`Saved document with _id: ${results.insertedId} to the database.`);
+                res.status(200).send("Data saved successfully");
+            }
+        });
+});
