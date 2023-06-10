efef9e9cdde7fc9294a9643152b66a200d8e0521
azimezn <117550865+azimezn@users.noreply.github.com>
2023-06-09 20:31:45 -0400

    emailjs finally works

--- assets/js/emailjs.js
+++ assets/js/emailjs.js
@@ -1,32 +1,34 @@
+(function () {
+    emailjs.init("iG9VxD9Wq1b0x012T");
+})();
+
 function sendEmail(event) {
     event.preventDefault();
 
     // get form data
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
+    const name = document.getElementById('nameInput').value;
+    const attendance = document.querySelector('input[name="attendance"]:checked').value;
+    const message = document.getElementById('messageText').value;
 
-    // Customize the email template ID and the service ID with your own values
     const templateParams = {
-      name: "azime",
-      email: "azimeznail",
-      message: "this is a message"
+        name: name,
+        attendance: attendance,
+        message: message
     };
 
-    // Customize the service ID and the template ID with your own values
-    emailjs.send('your_service_id', 'your_template_id', templateParams)
-      .then(function(response) {
-        console.log('Email sent successfully:', response.status, response.text);
-        alert('Thank you! Your message has been sent.');
-        // Clear form fields
-        document.getElementById('name').value = '';
-        document.getElementById('email').value = '';
-        document.getElementById('message').value = '';
-      }, function(error) {
-        console.error('Error sending email:', error);
-        alert('Oops! Something went wrong. Please try again later.');
-      });
-  }
+    emailjs.send('service_2yxx7o8', 'template_5z7i8fn', templateParams)
+        .then(function (response) {
+            console.log('Email sent successfully:', response.status, response.text);
+            alert('Thank you! Your message has been sent.');
+            // Clear form fields
+            document.getElementById('nameInput').value = '';
+            document.querySelector('input[name="attendance"]:checked').checked = false;
+            document.getElementById('messageText').value = '';
+        }, function (error) {
+            console.error('Error sending email:', error);
+            alert('Oops! Something went wrong. Please try again later.');
+        });
+}
 
-  // Attach the sendEmail function to the form's submit event
-  document.getElementById('contactForm').addEventListener('submit', sendEmail);
+// attach the sendEmail function to the form's submit event
+document.getElementById('contactForm').addEventListener('submit', sendEmail);
